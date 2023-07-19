c5b58ed9414fb4178bc734a5f7d9be683228460a
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2023-07-19 22:46:30 +0530

    codechef template updated

---
+++ codechef_template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int 
+
+#define rep(i,a,b)    for(ll i=a;i<b;i++)
+#define rrep(i,a,b)   for(ll i=a;i>=b;i--)
+#define e2nrep(i,a,b)  for(ll i=a;i<=b;i++)
+
+#define ff first
+#define sc second
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+void avinash_vermaa()   
+{
+   ll n,m,a,b,c,d,x,y,z,k,w,p,q,s;
+   ll flag=0,count=0,temp=0;
+   
+  	
+  	
+  	
+  }
+
+int main()
+{
+	
+	fastio();
+	int t; 
+	
+	cin>>t; while(t--)
+	
+	{
+			avinash_vermaa();	
+	}
+}
+
