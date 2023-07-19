753fc8d484486dca26461707a638bc4754dc95af
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2023-07-19 22:44:24 +0530

    Add files via upload

---
+++ cp_template.cpp
@@ -0,0 +1,60 @@
+// avinash_verma
+//code chef :- avinash_vermaa
+//github : -   avinash_vermaaa
+#include "bits/stdc++.h"
+#include "algorithm"
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+// using namespace chrono;
+ // using namespace __gnu_pbds;
+
+#define ll                  long long int
+// typedef ull              unsigned long long ;
+// typedef lld              long double ;
+
+#define rep(i,a,b)          for(ll i=a;i<b;i++)
+#define rrep(i,a,b)         for(ll i=b;i>=a;i--)
+#define e2nrep(i,a,b)       for(ll i=a;i<=b;i++)
+
+
+#define pb                  push_back
+#define ppb                 pop_back
+#define pf                  push_front
+#define all(x)              (x).begin(),(x).end()
+
+#define mp                  make_pair
+#define ff                  first
+#define ss                  second
+#define PI                  3.141592653589793238462
+
+#define tf2lo(x)           transform(str.begin(), str.end(), str.begin(), ::tolower)
+#define tf2up(x)           transform(str.begin(), str.end(), str.begin(), ::toupper)
+
+#define fastio()            ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+//User function template for C++
+
+void avinash_verma()
+{
+   ll n,m,a,b,c,d,x,y,z,k,w,p,q,s;
+   ll flag=0,count=0,temp=0;
+
+}
+
+
+
+int32_t main()
+{
+    fastio();  // cin cout tie NULL
+    
+    
+    
+ 
+
+
+
+    avinash_verma();
+ 
+}
---
+++ template_kickstart.cpp
@@ -0,0 +1,54 @@
+// avinash_verma
+//code chef :- avinash_vermaa
+//github : -   avinash_vermaaa
+#include "bits/stdc++.h"
+#include "algorithm"
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+// using namespace chrono;
+ // using namespace __gnu_pbds;
+
+#define ll                  long long int
+// typedef ull              unsigned long long ;
+// typedef lld              long double ;
+
+#define rep(i,a,b)          for(ll i=a;i<b;i++)
+#define rrep(i,a,b)         for(ll i=b;i>=a;i--)
+#define e2nrep(i,a,b)       for(ll i=a;i<=b;i++)
+
+
+#define pb                  push_back
+#define ppb                 pop_back
+#define pf                  push_front
+#define all(x)              (x).begin(),(x).end()
+
+#define mp                  make_pair
+#define ff                  first
+#define ss                  second
+#define PI                  3.141592653589793238462
+
+#define tf2lo(x)           transform(str.begin(), str.end(), str.begin(), ::tolower)
+#define tf2up(x)           transform(str.begin(), str.end(), str.begin(), ::toupper)
+
+#define fastio()            ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int main()
+{
+    ll t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cout<<"Case #"<<i<<": ";
+        
+       
+  
+    }
+    
+    return 0;
+}
+
+
+
+
