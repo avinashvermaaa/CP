e43796490d4959dfa1b65fa7d9d443e8aef8c4b4
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-03-09 17:14:43 +0530

    Add files via upload

---
+++ HackerRank/conditional_statements.cpp
@@ -0,0 +1,86 @@
+//avinash_verma
+//code chef :- avinash_vermaa
+//github : -   avinash_vermaaa
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+
+#define rep(i,a,b)     for(ll i=a;i<b;i++)
+#define rrep(i,a,b)    for(ll i=a;i>=b;i--)
+#define e2nrep(i,a,b)  for(ll i=a;i<=b;i++)
+
+#define pb                  push_back
+#define ppb                 pop_back
+#define pf                  push_front
+#define all(x)              (x).begin(),(x).end()
+
+#define ff first
+#define sc second   			 // INT_MAX = 2147483647 INT_MIN = -2147483648
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+void avinash_vermaa()
+{
+   // ll n,m,a,b,c,d,x,y,z,k,w,p,q,r,s,l,h;
+   // ll flag=0,count=0,temp=0;
+   
+    int number;
+    cin>>number;
+	if(number==1)
+	{
+		cout<<"one"<<endl;
+	}    
+	else if(number==2)
+	{
+		cout<<"two"<<endl;
+	}    
+	else if(number==3)
+	{
+		cout<<"three"<<endl;
+	}    
+	else if(number==4)
+	{
+		cout<<"four"<<endl;
+	}    
+	else if(number==5)
+	{
+		cout<<"five"<<endl;
+	}    
+	else if(number==6)
+	{
+		cout<<"six"<<endl;
+	}    
+	else if(number==7)
+	{
+		cout<<"seven"<<endl;
+	}    
+	else if(number==8)
+	{
+		cout<<"eight"<<endl;
+	}
+	else if(number==9)
+	{
+		cout<<"nine"<<endl;
+	}    
+	
+	else
+	{
+		cout<<"Greater than 9"<<endl;
+	}
+}
+
+int main()
+{
+	fastio();
+	ll t;
+	// cin>>t; while(t--)
+	{
+		avinash_vermaa();	
+	}
+}
+
