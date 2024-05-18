49b218b4e39acb0f6f1cda884a45c95bb012333c
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-05-19 03:47:25 +0530

    Create MORNINGRUN.cpp

---
+++ CodeChef/MORNINGRUN.cpp
@@ -0,0 +1,55 @@
+// https://www.codechef.com/problems/MORNINGRUN
+
+
+//avinash_verma
+//code chef :- avinash_vermaa
+//github : -   avinash_vermaaa
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+
+#define rep(i,a,b)     for(ll i=a;i<b;i++)
+#define rrep(i,a,b)    for(ll i=a;i>=b;i--)
+#define e2nrep(i,a,b)  for(ll i=a;i<=b;i++)
+
+#define pb                  push_back
+#define ppb                 pop_back
+#define pf                  push_front
+#define all(x)              (x).begin(),(x).end()
+
+#define ff first
+#define sc second   			 // INT_MAX = 2147483647 INT_MIN = -2147483648
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+void avinash_vermaa()
+{
+   ll n,m,a,b,c,d,x,y,z,k,w,p,q,r,s,l,h;
+   ll flag=0,count=0,temp=0;
+   
+   cin>>a>>b;
+   
+   a*= 2;
+   b*= 2;
+   a+= b;
+   
+   a>= 1000 ? cout<<"YES" : cout<<"NO";
+   
+   cout<<endl;
+   
+      
+}
+
+int main()
+{
+	fastio();
+	ll t;
+	// cin>>t; while(t--)
+	{
+		avinash_vermaa();	
+	}
+}
+
