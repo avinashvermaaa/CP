57e446a45be77fc114965bcc91ac06538c6b6137
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-05-19 03:46:08 +0530

    Create MONEYDOUBLE.cpp

---
+++ CodeChef/MONEYDOUBLE.cpp
@@ -0,0 +1,66 @@
+// https://www.codechef.com/problems/MONEYDOUBLE
+
+
+//avinash_verma
+//code chef :- avinash_vermaa
+//github : -   avinash_vermaaa
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+
+#define rep(i,a,b)     for(ll i=a;i<b;i++)
+#define rrep(i,a,b)    for(ll i=a;i>=b;i--)
+#define e2nrep(i,a,b)  for(ll i=a;i<=b;i++)
+
+#define pb                  push_back
+#define ppb                 pop_back
+#define pf                  push_front
+#define all(x)              (x).begin(),(x).end()
+
+#define ff first
+#define sc second   			 // INT_MAX = 2147483647 INT_MIN = -2147483648
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+void avinash_vermaa()
+{
+   ll n,m,a,b,c,d,x,y,z,k,w,p,q,r,s,l,h;
+   ll flag=0,count=0,temp=0;
+   
+   cin>>x>>y;
+   
+   flag = x;
+   
+   for(int i = 1;i<=y; i++)
+   {
+      if(flag <= 1000)
+      {
+         flag +=  1000; 
+      }
+      
+      else
+      {
+         flag *= 2;
+      }
+   }
+   
+   cout<<flag<<endl;
+   
+   
+   
+      
+}
+
+int main()
+{
+	fastio();
+	ll t;
+	cin>>t; while(t--)
+	{
+		avinash_vermaa();	
+	}
+}
+
