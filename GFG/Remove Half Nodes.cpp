2cb7fb2d6b5ec9b271fbfb711b1743339802836a
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-07-20 17:48:32 +0530

    Create Remove Half Nodes.cpp

---
+++ GFG/Remove Half Nodes.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/remove-half-nodes/1
+
+
+class Solution {
+  private:
+    Node* dfs(Node* root) {
+        if(!root) return NULL;
+        Node* left=dfs(root->left);
+        Node* right=dfs(root->right);
+        if(!left && !right) return root;
+        if(!left) return right;
+        else if(!right) return left;
+        root->left=left;
+        root->right=right;
+        return root;
+    }
+    
+public:
+    Node *RemoveHalfNodes(Node *root) {
+        Node *cur=dfs(root);
+        return cur;
+    }
+};
