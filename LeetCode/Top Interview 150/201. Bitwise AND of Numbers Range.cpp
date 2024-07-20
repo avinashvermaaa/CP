658681d96ad5bb344b3d08be857214714b2776cb
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-07-20 21:46:47 +0530

    Update 201. Bitwise AND of Numbers Range.cpp

--- LeetCode/Top Interview 150/201. Bitwise AND of Numbers Range.cpp
+++ LeetCode/Top Interview 150/201. Bitwise AND of Numbers Range.cpp
@@ -11,3 +11,21 @@ public:
         return (left << cnt);
     }
 };
+
+
+-----------------------------------------------------------
+
+    
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+         while(right>left)
+           {
+            right = right & right-1;
+           }
+
+        return left&right;
+        
+    }
+};
