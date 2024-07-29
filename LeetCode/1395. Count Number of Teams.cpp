2bb552a0cbca1b4c92742acb525faf2edd8a8c78
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-07-30 00:17:09 +0530

    Create 1395. Count Number of Teams.cpp

---
+++ LeetCode/1395. Count Number of Teams.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+    
+    
+public:
+    int numTeams(vector<int>& rating) {
+        int r = 0;
+        const int n = rating.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                for (int k = 0; k < j; ++k) {
+                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||  (rating[i] > rating[j] && rating[j] > rating[k])) {
+                        ++r;
+                    }
+                }
+            }
+        }
+        return r;
+        
+    }
+};
