7ae7e588e3a9badecbedf0a96442594934e30f3d
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-08-12 23:00:24 +0530

    Update 48. Rotate Image.cpp

--- LeetCode/48. Rotate Image.cpp
+++ LeetCode/48. Rotate Image.cpp
@@ -17,20 +17,11 @@ public:
       for(int i =0;i<n;i++)
       {
          reverse(matrix[i].begin(),matrix[i].end());
-      }
-      
-    //   return matrix;
-      
-        
+      }   
    }
 };
 
-
-
-
-auto init = [](){
-   
-   
+auto init = [](){   
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
    cout.tie(nullptr);
