ef194fc499b6bfcb24619eb6b8518c841dc69728
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-08-17 05:18:55 +0530

    Create 624. Maximum Distance in Arrays.cpp

---
+++ LeetCode/624. Maximum Distance in Arrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int smallest = arrays[0][0];
+        int biggest = arrays[0].back();
+        int max_distance = 0;
+
+        for (int i = 1; i < arrays.size(); ++i) {
+            max_distance = max(max_distance, abs(arrays[i].back() - smallest));
+            max_distance = max(max_distance, abs(biggest - arrays[i][0]));
+            smallest = min(smallest, arrays[i][0]);
+            biggest = max(biggest, arrays[i].back());
+        }
+
+        return max_distance;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
