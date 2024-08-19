376ac2c198192e5325c1656cf65c9101abbc7d64
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-08-19 23:30:57 +0530

    Create 650. 2 Keys Keyboard.cpp

---
+++ LeetCode/650. 2 Keys Keyboard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        
+        return steps;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
