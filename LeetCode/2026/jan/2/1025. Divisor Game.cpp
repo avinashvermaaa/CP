660d4d61430996f569147a3ece4012d1ab65e44f
Avinash Verma <105039208+avinashvermaaa@users.noreply.github.com>
2026-01-01 23:36:29 +0530

    Add solution for Divisor Game problem

---
+++ LeetCode/2026/jan/2/1025. Divisor Game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return !(n & 1);
+    }
+};
+
+int init = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 'c';
+}();
