a6bd8a4545598ec42b25c2ed1ccde02c9c3292fe
Avinash <105039208+avinashvermaaa@users.noreply.github.com>
2026-03-01 00:10:56 +0530

    Implement function to find closest pair from two arrays

---
+++ GFG/GFG_X_NPCI/Find the closest pair from two arrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> findClosestPair(vector<int> &arr1, vector<int> &arr2, int x) {
+        int n = arr1.size();
+        int m = arr2.size();
+        
+        int i = 0;
+        int j = m - 1;
+        
+        int minDiff = INT_MAX;
+        vector<int> result(2);
+        
+        while(i < n && j >= 0) {
+            int sum = arr1[i] + arr2[j];
+            int diff = abs(sum - x);
+            
+            if(diff < minDiff) {
+                minDiff = diff;
+                result[0] = arr1[i];
+                result[1] = arr2[j];
+            }
+            
+            if(sum > x) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        
+        return result;
+    }
+};
